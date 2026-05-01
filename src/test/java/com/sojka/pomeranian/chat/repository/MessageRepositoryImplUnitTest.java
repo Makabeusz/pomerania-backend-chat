4023764bde3886dec354d25c3d871725f04f11b9
Mateusz Sójka <mateuszsojka@proton.me>
2026-05-01 15:29:11 +0200

    deploy 0.2.2

--- src/test/java/com/sojka/pomeranian/chat/repository/MessageRepositoryImplUnitTest.java
+++ src/test/java/com/sojka/pomeranian/chat/repository/MessageRepositoryImplUnitTest.java
@@ -51,7 +51,7 @@ class MessageRepositoryImplUnitTest {
         when(mockedSession.execute(any(SimpleStatement.class))).thenThrow(new RuntimeException("unexpected error"));
         assertThatThrownBy(() -> repository.save(Message.builder().roomId("user1:user2").content("dummy").username("dummy").build()))
                 .isExactlyInstanceOf(AstraException.class)
-                .hasMessage("Failed to execute save, for: Message(roomId=user1:user2, createdAt=null, profileId=null, username=dummy, recipientProfileId=null, recipientUsername=null, content=dummy, resourceId=null, resourceType=null, thumbnailId=null, editedAt=null, readAt=null, metadata=null)")
+                .hasMessage("Failed to execute save, for: Message(roomId=user1:user2, createdAt=null, profileId=null, username=dummy, recipientProfileId=null, recipientUsername=null, content=dummy, resourceId=null, resourceType=null, resourceHeight=null, resourceWidth=null, thumbnailId=null, editedAt=null, readAt=null, metadata=null)")
                 .hasCauseExactlyInstanceOf(RuntimeException.class)
                 .hasRootCauseMessage("unexpected error");
     }
